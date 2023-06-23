4569093fec126c75c2b70b3ae7e10e4b8e663a77
Holly Stefan <hmbullis828@gmail.com>
2023-06-22 18:57:41 -0700

    css tweaks

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,17 +3,18 @@ import ReactDOM from "react-dom";
 const { useState } = React;
 import OutfitsAndRelatedItems from "./relatedItemComponents/OutfitsAndRelatedItems.jsx";
 import Overview from "./Overview/Overview.jsx";
+import RatingsAndReview from "./ratingsReviews/ratingsAndReview.jsx";
 const App = () => {
   const [currentProduct, setCurrentProduct] = useState({});
 
   return (
     <>
       <div>hello world, mars</div>
-      <div>
-
+      <div class="section">
         <Overview />
+      </div>
+      <div class="section">
         <OutfitsAndRelatedItems currentProduct={currentProduct} />
-
       </div>
       <div class="section">Questions and Answers</div>
       <div class="section">
