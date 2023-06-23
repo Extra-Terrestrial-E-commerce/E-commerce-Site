35abd8cd0e962b48fc9434109ee02ef4f4cd57d9
Oscar Carvente <oscar.carvente@gmail.com>
2023-06-22 17:33:29 -0700

    Layout for Product Info complete

--- client/src/components/Overview/components/ProductInfo.jsx
+++ client/src/components/Overview/components/ProductInfo.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const ProductInfo = () => {
   return (
-    <div>ProductInfo</div>
+    <div className='overview'>
+      <span>star rating</span>
+      <a href=''>Read all reviews</a>
+      <div>Category</div>
+      <h3>Product Name</h3>
+      <p>price</p>
+    </div>
   )
 }
 
--- client/src/components/Overview/components/ProductOverview.jsx
+++ client/src/components/Overview/components/ProductOverview.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const ProductOverview = () => {
   return (
-    <div>ProductOverview</div>
+    <div>
+      <h3>ProductOverview</h3>
+      <p>Product Overview - This free form text field may exist on some items.
+         If it is available it should be displayed
+      </p>
+      <div>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
   )
 }
 
