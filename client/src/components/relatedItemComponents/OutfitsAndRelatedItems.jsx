ff2118373d7529554f5b938b672e2b1bcb3ac42c
Patrick <patrick.mayeda@gmail.com>
2023-06-22 20:03:41 -0600

    config.example.js set up, api calls functional

--- client/src/components/relatedItemComponents/OutfitsAndRelatedItems.jsx
+++ client/src/components/relatedItemComponents/OutfitsAndRelatedItems.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import RelatedItems from "./RelatedItems.jsx";
 import OutfitItems from "./OutfitItems.jsx";
-const { useState } = React;
+const { useState, useEffect } = React;
+import apiClient from '../config/config.js';
 
 const OutfitsAndRelatedItems = ( {currentProduct} ) => {
+
+  useEffect(() => {
+    apiClient.get('/products', { params: {count: 20} } )
+      .then((data) => {
+        console.log(data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }, [])
+
+
   return (
     <>
       <div>
