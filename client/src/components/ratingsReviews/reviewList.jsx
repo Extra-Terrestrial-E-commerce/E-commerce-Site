3313bf290c80a7d3f488c143f0d11f3163d41293
Holly Stefan <hmbullis828@gmail.com>
2023-06-23 15:25:29 -0700

    working on api requests for reviews

--- client/src/components/ratingsReviews/reviewList.jsx
+++ client/src/components/ratingsReviews/reviewList.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReviewListItem from "./reviewListItem.jsx";
 import WriteReview from "./writeReview.jsx";
 
-const ReviewList = () => {
+const ReviewList = ({currentReviews}) => {
+  console.log("reviewList", currentReviews);
+
+
   return (
     <div>
       <h2>248 reviews, sorted by relevance</h2>
       <p>Review List</p>
-      <ReviewListItem />
-      <ReviewListItem />
+      {/* {currentReviews.results.map((review) => {
+        <ReviewListItem review={review}/>
+      })} */}
       <div class="row">
         <button>MORE REVIEWS</button>
         <button>ADD A REVIEW</button>
--- client/src/components/ratingsReviews/reviewListItem.jsx
+++ client/src/components/ratingsReviews/reviewListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const ReviewListItem = () => {
+const ReviewListItem = ({review}) => {
+
   return (
     <div>
       <p>Imagine Stars Here</p>
