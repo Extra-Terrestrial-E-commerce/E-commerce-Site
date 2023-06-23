3775bb7166b8025102ef2f9d237ba32c64ce631d
Holly Stefan <hmbullis828@gmail.com>
2023-06-23 15:25:29 -0700

    working on api requests for reviews

--- client/src/components/ratingsReviews/ratingsAndReview.jsx
+++ client/src/components/ratingsReviews/ratingsAndReview.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import RatingsBreakdown from "./ratingsBreakdown.jsx";
 import ReviewList from "./reviewList.jsx"
+const { useState, useEffect } = React;
+import apiClient from '../config/config.js';
 
-const RatingsAndReview = () => {
+const RatingsAndReview = ({currentProduct}) => {
+  const [currentReviews, setCurrentReviews] = useState({});
+
+
+  const reviewparams = {
+    params : {
+      product_id: 40344
+
+    }
+  }
+
+  useEffect(() => {
+    apiClient.get('/reviews/', reviewparams )
+      .then((data) => {
+        setCurrentReviews(data);
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      apiClient.get('/reviews/meta', reviewparams )
+      .then((data) => {
+        console.log("review meta", data);
+
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }, [])
+
+
+
   return (
     <>
     <div class="row">
@@ -10,7 +43,7 @@ const RatingsAndReview = () => {
         <RatingsBreakdown />
       </div>
       <div class="twoThirds">
-        <ReviewList />
+        <ReviewList currentReviews={currentReviews}/>
       </div>
 
     </div>
--- client/src/components/ratingsReviews/writeReview.jsx
+++ client/src/components/ratingsReviews/writeReview.jsx
@@ -1,6 +1,47 @@
 import React from "react";
+const { useState, useEffect } = React;
+import apiClient from '../config/config.js';
 
 const WriteReview = () => {
+
+  // const [addReviewParams, setAddReviewParams] = useState {
+  //   product_id: 0,
+  //   rating: 0,
+  //   summary: "",
+  //   body: "",
+  //   recommend: true,
+  //   name: "",
+  //   email: "",
+  //   photos: [],
+  //   characteristics: {}
+  // }
+
+  const addReviewParams = {
+      product_id: 40344,
+      rating: 3.5,
+      summary: "I liked the camo onsie",
+      body: "it was a great fit for my baby",
+      recommend: true,
+      name: "satisfiedCustomer1",
+      email: "example@example.com",
+      photos: [],
+      characteristics: {}
+    }
+
+
+  const addReview = (params) => {
+    apiClient.post('/reviews/', params )
+        .then((data) => {
+          setCurrentReviews(data);
+
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  };
+
+  addReview(addReviewParams);
+
   return (
     <div>
       <form>
