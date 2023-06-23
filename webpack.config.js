b0994dee2ed946789f77c06f3e67b4d8208a41dc
Patrick <patrick.mayeda@gmail.com>
2023-06-22 20:03:41 -0600

    config.example.js set up, api calls functional

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /(?:\/|\\)(config|node_modules)(?:\/|\\)/,
         use: {
           loader: "babel-loader",
         },
