f77373e2aeafdac3d57fe39e4692db357646c728
Holly Stefan <hmbullis828@gmail.com>
2023-06-25 13:41:48 -0700

    set min and max characters for review input

--- client/src/components/ratingsReviews/writeReview.jsx
+++ client/src/components/ratingsReviews/writeReview.jsx
@@ -46,7 +46,9 @@ const WriteReview = ({currentProduct, closeModal}) => {
         <br />
 
         <textarea placeholder="description"
-        onChange={(e)=>{setAddReviewParams.body(e.target.value)}}></textarea>
+        minLength="50"
+        maxLength="1000"
+        onChange={(e)=>{setAddReviewParams(addReviewParams.body= e.target.value)}}></textarea>
         <br/>
 
         <input placeholder="name"
