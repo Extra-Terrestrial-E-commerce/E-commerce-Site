b980370129d3e01527f6d74be23386299a22ad2f
Oscar Carvente <oscar.carvente@gmail.com>
2023-06-25 17:04:55 -0700

    Add green checkmark to selected style on ProductStyles component

--- client/src/components/Overview/components/ProductStyle.jsx
+++ client/src/components/Overview/components/ProductStyle.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import Style from './Style.jsx'
 const ProductStyle = ({styles}) => {
   const[selectedStyle, setSelectedStyle] = React.useState({});
+
+  React.useEffect (() => {
+    if (styles.length) {
+      var defaultStyle = styles.find(style => style['default?']);
+      setSelectedStyle(defaultStyle);
+    }
+  }, [styles])
+  const setStyle = (style) => {
+    setSelectedStyle (style);
+  }
+
   return (
     <div className ='overview'>
-      <span>Style > </span>
-      <span>Style Selected</span>
+      <span>Style &#62; </span>
+      <span>{selectedStyle.name}</span>
       <div className='style-display'>
-        {styles.length && styles.map(style => <Style style ={style} />)}
+        {styles.length && styles.map(style =>
+          <Style style ={style} setStyle = {setStyle} selected ={style.style_id === selectedStyle.style_id}/>
+        )}
       </div>
 
     </div>
