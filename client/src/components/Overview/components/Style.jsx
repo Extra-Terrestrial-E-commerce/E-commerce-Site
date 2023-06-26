876e4d4c4ffb6623ce0c55c797d169962ceafa85
Oscar Carvente <oscar.carvente@gmail.com>
2023-06-25 17:04:55 -0700

    Add green checkmark to selected style on ProductStyles component

--- client/src/components/Overview/components/Style.jsx
+++ client/src/components/Overview/components/Style.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {ImCheckmark} from 'react-icons/im'
 
-const Style = ({style}) => {
-  console.log('this is the style: ', style);
+const Style = ({style, setStyle, selected}) => {
+
+
+
+  const handleClick = () => {
+    setStyle(style);
+  }
+
   return (
-    <img className='style-thumbnail' src={style.photos[0].thumbnail_url}/>
+    <div className='style-parent'>
+      {selected &&
+        <ImCheckmark className='checkmark'/>
+      }
+      <img className='style-thumbnail' src={style.photos[0].thumbnail_url} onClick ={handleClick}/>
+    </div>
   )
 }
 
