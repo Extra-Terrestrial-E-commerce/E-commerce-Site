3e074e3bea4f48c678da729ed5d32f84a5a4176a
Holly Stefan <hmbullis828@gmail.com>
2023-06-27 11:27:39 -0700

    mark reviews as helpful

--- client/src/components/ratingsReviews/reviewListItem.jsx
+++ client/src/components/ratingsReviews/reviewListItem.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import AllStars from "../Stars/AllStars.jsx";
+import apiClient from '../config/config.js';
+const { useState } = React;
 
 
 
 const ReviewListItem = ({review}) => {
+  const [helpfulStatus, setHelpfulStatus] = useState(false);
 
   const getMonthName = (monthNumber) => {
     const date2 = new Date();
@@ -30,7 +33,33 @@ const ReviewListItem = ({review}) => {
     return text;
   }
 
-  // console.log("review", review);
+  const handleReviewHelpful = (e) => {
+    setHelpfulStatus(true);
+    apiClient.put(`/reviews/${review.review_id}/helpful`, {params: {review_id: review.review_id} } )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+
+  const handleReviewReport = (e) => {
+    setHelpfulStatus(true);
+    console.log("helpfulStatus", helpfulStatus);
+    apiClient.put(`/reviews/${review.review_id}/report`, {params: {review_id: review.review_id} } )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+
+   console.log("review", review);
+
 
   return (
     <div>
@@ -48,8 +77,14 @@ const ReviewListItem = ({review}) => {
       {review.recommend && <p>I recommend this product</p>}
       <div class="row">
         <p>Helpful?</p>
-        <button>Yes</button>
-        <button>No</button>
+        {helpfulStatus && <p>Thank you</p>}
+        {helpfulStatus === false &&
+        <>
+          <button onClick={handleReviewHelpful}>Yes </button>
+          <button onClick={handleReviewReport}>No </button>
+        </>
+        }
+
       </div>
       <p>_______________________________</p>
     </div>
