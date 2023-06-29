4fab1146ed06a3ac7a49efde0ae02a67a9be6313
Holly Stefan <hmbullis828@gmail.com>
2023-06-29 00:08:53 -0700

    added read more button to long reviews and fixed ratings bar when there are over 100 ratings

--- client/src/components/ratingsReviews/ratingsBreakdown.jsx
+++ client/src/components/ratingsReviews/ratingsBreakdown.jsx
@@ -23,7 +23,14 @@ const RatingsBreakdown = ({reviewMeta, filterHandler}) => {
     return average.toFixed(1);
   }
 
-
+  const getTotalRatings = (ratings) => {
+    const five = Number(ratings['5']);
+    const four = Number(ratings['4']);
+    const three = Number(ratings['3']);
+    const two = Number(ratings['2']);
+    const one = Number(ratings['1']);
+    return five+four+three+two+one;
+  }
 
   return (
     <div>
@@ -40,23 +47,28 @@ const RatingsBreakdown = ({reviewMeta, filterHandler}) => {
 
         <div class="row">
           <button onClick={()=>{filterHandler(5)}}>5 stars </button>
-          <RatingsBar completed={reviewMeta.ratings['5']}/>
+          <RatingsBar completed={reviewMeta.ratings['5']/getTotalRatings(reviewMeta.ratings)*100}
+          actualNumber={reviewMeta.ratings['5']}/>
         </div>
         <div class="row">
           <button onClick={()=>{filterHandler(4)}}>4 stars </button>
-          <RatingsBar completed={reviewMeta.ratings['4']}/>
+          <RatingsBar completed={reviewMeta.ratings['4']/getTotalRatings(reviewMeta.ratings)*100}
+          actualNumber={reviewMeta.ratings['4']}/>
         </div>
         <div class="row">
           <button onClick={()=>{filterHandler(3)}}>3 stars </button>
-          <RatingsBar completed={reviewMeta.ratings['3']}/>
+          <RatingsBar completed={reviewMeta.ratings['3']/getTotalRatings(reviewMeta.ratings)*100}
+          actualNumber={reviewMeta.ratings['3']}/>
         </div>
         <div class="row">
           <button onClick={()=>{filterHandler(2)}}>2 stars </button>
-          <RatingsBar completed={reviewMeta.ratings['2']}/>
+          <RatingsBar completed={reviewMeta.ratings['2']/getTotalRatings(reviewMeta.ratings)*100}
+          actualNumber={reviewMeta.ratings['2']}/>
         </div>
         <div class="row">
           <button onClick={()=>{filterHandler(1)}}>1 star </button>
-          <RatingsBar completed={reviewMeta.ratings['1']}/>
+          <RatingsBar completed={reviewMeta.ratings['1']/getTotalRatings(reviewMeta.ratings)*100}
+          actualNumber={reviewMeta.ratings['1']}/>
         </div>
         <br/>
         < CharacteristicsBars characteristics={reviewMeta.characteristics}/>
