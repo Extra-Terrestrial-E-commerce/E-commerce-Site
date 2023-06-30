202f8e4371514ed3d113157fe8e0a10cd4c299b0
Patrick <patrick.mayeda@gmail.com>
2023-06-29 22:03:09 -0600

    testing for stars complete

--- client/src/components/relatedItemComponents/tests/relatedItemsTests.test.js
+++ client/src/components/relatedItemComponents/tests/relatedItemsTests.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, fireEvent, screen, act } from '@testing-library/react';
+const { useState } = React;
+import { render, fireEvent, screen, act, waitFor, cleanup } from '@testing-library/react';
 import App from '../../App.jsx';
 import RelatedItems from '../relatedItems/RelatedItems.jsx';
 import ComparisonModal from '../relatedItems/ComparisonModal.jsx';
@@ -45,6 +46,155 @@ describe('should display related items', () => {
     expect(category).toBeTruthy();
     expect(price).toBeTruthy();
     expect(stars).toBeTruthy();
+  })
+
+  it('should toggle visibility of comparison modal', async () => {
+    function TestWrapper({ initialIsAnyComparing }) {
+      const [isAnyComparing, setIsAnyComparing] = useState(initialIsAnyComparing);
+      return (
+        <RelatedCard
+          product={relatedItems[0]}
+          isAnyComparing={isAnyComparing}
+          setIsAnyComparing={setIsAnyComparing}
+        >
+          <ComparisonModal product={relatedItems[0]} currentProduct={currentProduct} />
+        </RelatedCard>
+      );
+    }
+
+    await act(async () => {
+      render(
+        <TestWrapper initialIsAnyComparing={false} />
+      );
+    });
+
+    var modalContainer = screen.getByTestId("comparisonModalContainer");
+    var currentStyle = window.getComputedStyle(modalContainer);
+    expect(currentStyle._values.display).toBe('none');
+
+    const starButton = screen.getByRole('openCompare');
+    fireEvent.click(starButton);
+    var currentStyle = window.getComputedStyle(modalContainer);
+    expect(currentStyle._values.display).toBe('flex');
+
+    fireEvent.click(starButton);
+    var currentStyle = window.getComputedStyle(modalContainer);
+    expect(currentStyle._values.display).toBe('none');
+  })
+
+  it('should make an api call in relatedCard to get the review data, set the starRating w/ averaged rating', async () => {
+
+    const setState = jest.fn();
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementationOnce(initState => [initState, setState]);
+
+    apiClient.get.mockResolvedValue({data: review})
+    await act(async () => {
+      render(
+         <RelatedCard product={relatedItems[3]} />
+      );
+    });
+
+    const aggregate = (objectOfReviews) => {
+      var count = 0;
+      var total = 0;
+      for (let key in objectOfReviews) {
+        count = count +  parseInt(objectOfReviews[key]);
+        total = total + (key * objectOfReviews[key]);
+      }
+      return [count, total];
+    }
+    var aggregatedData = aggregate(review.ratings);
+    var stars = aggregatedData[1]/aggregatedData[0];
+    var stars = stars.toFixed(2);
+    expect(setState).toHaveBeenCalledWith(stars);
+
+  })
+
+})
+
+describe('should display a rating as an image of filled stars', () => {
+  it('should always consist of five stars', () => {
+    render(<AllStars rating={3.6} size={10} />)
+    var stars = screen.getAllByRole('oneStar')
+    expect(stars.length).toBe(5);
+  })
+
+  it('should fill a star appropriately', () => {
+    const findVertical = (pointSequence) => {
+      var vertical;
+      for (var i = 0; i < pointSequence.length - 1; i++) {
+        if (pointSequence[i].props.x === pointSequence[i + 1].props.x) {
+          vertical = pointSequence[i].props.x
+          break
+        }
+      }
+      return vertical;
+    }
+
+    render(<OneStar percentFill={1} size={10}/>)
+    var canvas = screen.getByRole('oneStar');
+    var ctx = canvas.getContext('2d');
+    var events = ctx.__getEvents();
+    var lastEvent = events[events.length - 1]
+    expect(lastEvent.type).toEqual('fill');
+    var path = lastEvent.props.path;
+    path = path.slice(1, path.length - 1);
+    var fullStarVertical = findVertical(path);
+    expect(fullStarVertical).toBe(undefined);
+    cleanup();
+
+    render(<OneStar percentFill={0} size={10}/>)
+    var canvas = screen.getByRole('oneStar');
+    var ctx = canvas.getContext('2d');
+    var events = ctx.__getEvents();
+    var lastEvent = events[events.length - 1]
+    expect(lastEvent.type).toEqual('stroke');
+    var path = lastEvent.props.path;
+    path = path.slice(1, path.length - 1);
+    var fullStarVertical = findVertical(path);
+    expect(fullStarVertical).toBe(undefined);
+    cleanup();
+
+    render(<OneStar percentFill={.25} size={10}/>)
+    var canvas = screen.getByRole('oneStar');
+    var ctx = canvas.getContext('2d');
+    var events = ctx.__getEvents();
+    var lastEvent = events[events.length - 1]
+    expect(lastEvent.type).toEqual('fill');
+    var path = lastEvent.props.path;
+    path = path.slice(1, path.length - 1);
+    var fullStarVertical = findVertical(path);
+    expect(fullStarVertical).toBe(42);
+    cleanup();
+
+    render(<OneStar percentFill={.5} size={10}/>)
+    var canvas = screen.getByRole('oneStar');
+    var ctx = canvas.getContext('2d');
+    var events = ctx.__getEvents();
+    var lastEvent = events[events.length - 1]
+    expect(lastEvent.type).toEqual('fill');
+    var path = lastEvent.props.path;
+    path = path.slice(1, path.length - 1);
+    var fullStarVertical = findVertical(path);
+    expect(fullStarVertical).toBe(50);
+    cleanup();
+
+    render(<OneStar percentFill={.75} size={10}/>)
+    var canvas = screen.getByRole('oneStar');
+    var ctx = canvas.getContext('2d');
+    var events = ctx.__getEvents();
+    var lastEvent = events[events.length - 1]
+    expect(lastEvent.type).toEqual('fill');
+    var path = lastEvent.props.path;
+    path = path.slice(1, path.length - 1);
+    var fullStarVertical = findVertical(path);
+    expect(fullStarVertical).toBe(58);
+    cleanup();
+
+
+
   })
 
   it('should toggle visibility of comparison modal', async () => {
