54b618cd06ca7f33129b256f3fba30d9241dfe29
Oscar Carvente <oscar.carvente@gmail.com>
2023-07-02 16:14:54 -0700

    After zooming in image pans in the direction of the mouse

--- client/src/components/Overview/components/ExpandedView.jsx
+++ client/src/components/Overview/components/ExpandedView.jsx
@@ -2,26 +2,44 @@ import React from 'react';
 import ImgStyleIcon from './ImgStyleIcon.jsx';
 const ExpandedView = ({imageUrl, selectNext, selectBefore, enableBefore, enableNext, thumbNailGallery}) => {
   const [zoomIn, setZoomIn] = React.useState(false);
-
-  const zoomInStyle = () => {
-    if (zoomIn) {
-      return {transform: 'scale(2.5)'};
-    }
-  }
+  const [touch, setTouch] = React.useState({x:0, y:0});
+  const [movement, setMovement] = React.useState({deltaX: 0, deltaY:0})
+  const zoomInStyle = {
+    'transform-origin':`${touch.x}px ${touch.y}px`,
+    transform: `scale(2.5) translateX(${movement.deltaX * -1}px) translateY(${movement.deltaY * -1}px)`
+  };
   const expandedThumbnailStyles = {
     display:'flex',
     flexDirection: 'row'
   }
+
+  const handleClick = (e) => {
+    setTouch({x:e.clientX, y: e.clientY});
+    setZoomIn(!zoomIn);
+  }
+
+  const handleMouseMovement = (e) => {
+
+    setMovement({deltaX: e.clientX - touch.x, deltaY: e.clientY - touch.y})
+  }
+
   return(
     <div>
       {enableBefore && !zoomIn&& <button onClick ={selectBefore}>left</button> }
       <div className='image-container'>
-        <img
-          id ={zoomIn ? 'main-img-zoom-in': 'main-img-zoom-out'}
+        {zoomIn ? <img
+          className ='main-img-zoom-in'
           src ={imageUrl}
-          style={zoomInStyle()}
+          style={zoomInStyle}
           onClick={()=> setZoomIn(!zoomIn)}
+          onMouseMove={handleMouseMovement}
+        /> :
+        <img
+          className ='main-img-zoom-out'
+          src ={imageUrl}
+          onClick={handleClick}
         />
+        }
       </div>
 
       {enableNext && !zoomIn&& <button onClick ={selectNext}>right</button>}
