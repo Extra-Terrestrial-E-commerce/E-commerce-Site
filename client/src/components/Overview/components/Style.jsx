667562845fa6393cda5acbcfce8a03592a5e4ab2
Oscar Carvente <oscar.carvente@gmail.com>
2023-07-08 09:48:46 -0700

    Removed react-icons

--- client/src/components/Overview/components/Style.jsx
+++ client/src/components/Overview/components/Style.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {ImCheckmark} from 'react-icons/im'
 
 const Style = ({style, setStyle, selected}) => {
 
   return (
     <div className='style-parent'>
       {selected &&
-        <ImCheckmark className='checkmark'/>
+        <div className='checkmark'>&#10003;</div>
       }
       <img className='style-thumbnail' src={style.photos[0].thumbnail_url} onClick ={()=> setStyle(style)}/>
     </div>
